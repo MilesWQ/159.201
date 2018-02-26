6f7d1a14adaf534b95bd005620c0c5bb98bc371c
Miles <qwan442@gmail.com>
2018-02-27 10:10:53 +1300

    revise tree

--- Trees/Tree.cpp
+++ Trees/Tree.cpp
@@ -44,15 +44,36 @@ void PreOrder(Tree<T> *_root)
     PreOrder(_root->GetRight());
 }
 
+template<class T>
+void InOrder(Tree<T> *_root){
+    if(_root==nullptr){
+        return;
+    }
+    InOrder(_root->GetLeft());
+    cout<<_root->GetRootData()<<" ";
+    InOrder(_root->GetRight());
+}
+
+template<class T>
+void PostOrder(Tree<T> *_root){
+    if(_root == nullptr){
+        return;
+    }
+    PostOrder(_root->GetLeft());
+    PostOrder(_root->GetRight());
+    cout<<_root->GetRootData()<<" ";
+}
 int main()
 {
-    Tree<char> *t1 = new Tree<char>('D',nullptr,nullptr);
-    Tree<char> *t2 = new Tree<char>('E',nullptr,nullptr);
-    Tree<char> *t3 = new Tree<char>('B',t1,t2);
-    Tree<char> *t4 = new Tree<char>('F',nullptr,nullptr);
-    Tree<char> *t5 = new Tree<char>('G',nullptr,nullptr);
-    Tree<char> *t6 = new Tree<char>('C',t4,t5);
-    Tree<char> *tree = new Tree<char>('A',t3,t6);
-    PreOrder(tree);
+    Tree<char> *t1 = new Tree<char>('H',nullptr,nullptr);
+    Tree<char> *t2 = new Tree<char>('D',nullptr,nullptr);
+    Tree<char> *t3 = new Tree<char>('E',t1,nullptr);
+    Tree<char> *t4 = new Tree<char>('B',t2,t3);
+    Tree<char> *t5 = new Tree<char>('I',nullptr,nullptr);
+    Tree<char> *t6 = new Tree<char>('G',t5,nullptr);
+    Tree<char> *t7 = new Tree<char>('F',nullptr,nullptr);
+    Tree<char> *t8 = new Tree<char>('C',t7,t6);
+    Tree<char> *tree = new Tree<char>('A',t4,t8);
+    PostOrder(tree);
     return 0;
 }
